cb4ca7d45221992a5a0e333defdc5a818c71c0af
bgargy <138105909+bgargy@users.noreply.github.com>
2024-01-17 19:24:12 +0530

    8 passed 3 failed

--- src/index.test.js
+++ src/index.test.js
@@ -1,12 +1,15 @@
 const { lru } = require('./index.js');
-
-const _ = require('lodash');
+const {ttl} = require('./index.js');
 
 describe('LRUCache', () => {
   let cache;
 
   beforeEach(() => {
     cache = new lru();
+  });
+
+  afterEach(() => {
+    jest.clearAllTimers(); 
   });
 
   test('get returns undefined for non-existent key', () => {
@@ -24,8 +27,8 @@ describe('LRUCache', () => {
     const obj = { key: 'value' };
     cache.put('key2', obj);
     const result = cache.get('key2');
-    expect(result).toEqual(obj); 
-    expect(result).not.toBe(obj); 
+    expect(result).toEqual(obj);
+    expect(result).not.toBe(obj);
   });
 
   test('put and get work correctly with object values and cloning disabled', () => {
@@ -33,28 +36,101 @@ describe('LRUCache', () => {
     cache = new lru(1024, false);
     cache.put('key3', obj);
     const result = cache.get('key3');
-    expect(result).toBe(obj); 
+    expect(result).toBe(obj);
   });
 
   test('put removes least recently used item when exceeding size', () => {
     cache = new lru(2);
     cache.put('key4', 'value4');
     cache.put('key5', 'value5');
-    cache.put('key6', 'value6'); 
+    cache.put('key6', 'value6');
+    console.log(cache);
+    jest.advanceTimersByTime(1000);
+    console.log(cache);
     const result = cache.get('key4');
     expect(result).toBeUndefined();
   });
+  
 
   test('put and get work correctly with timestamps updated', () => {
     jest.useFakeTimers();
     cache.put('key7', 'value7');
-    jest.advanceTimersByTime(1000);
+    jest.advanceTimersByTime(2000);
     const resultBefore = cache.get('key7');
     expect(resultBefore).toBe('value7');
-    
+
     cache.put('key7', 'newvalue7');
     jest.advanceTimersByTime(1000);
     const resultAfter = cache.get('key7');
     expect(resultAfter).toBe('newvalue7');
+  });
+});
+
+describe('Cache TTL', () => {
+  let cache;
+
+  beforeEach(() => {
+    cache = new ttl();
+  });
+
+  it('should retrieve a cached value', () => {
+    const key = 'testKey';
+    const value = 'testValue';
+
+    cache.put(key, value);
+
+    const result = cache.get(key);
+
+    expect(result).toBe(value);
+  });
+
+  it('should not retrieve an expired value', async () => {
+    const key = 'testKey';
+    const value = 'testValue';
+
+    cache.put(key, value, 1);
+
+    jest.advanceTimersByTime(1100);
+
+    const result = cache.get(key);
+    expect(result).toBeUndefined();
+  });
+
+  it('should clone the value when retrieving if clone is set to true', () => {
+    const key = 'testKey';
+    const originalObject = { nested: { prop: 'value' } };
+
+    cache.put(key, originalObject);
+
+    const result = cache.get(key);
+
+    expect(result).toEqual(originalObject);
+    expect(result).not.toBe(originalObject);
+  });
+
+  it('should not clone the value when retrieving if clone is set to false', async() => {
+    const key = 'testKey';
+    const originalObject = { nested: { prop: 'value' } };
+
+    const nonCloningCache = new ttl(60, false);
+    nonCloningCache.put(key, originalObject);
+
+    const result = nonCloningCache.get(key);
+
+    expect(result).toBe(originalObject);
+    expect(result).not.toBe(originalObject); 
+  });
+
+  it('should delete a cached value', () => {
+    const key = 'testKey';
+    const value = 'testValue';
+
+    cache.put(key, value);
+
+    cache.delete(key);
+
+    const result = cache.get(key);
+
+    expect(result).toBeUndefined();
   });
 });

661a86358d8780c3237dfcb27e233b7dca7ab367
Prashant Gupta <antshpra@gmail.com>
2024-02-05 20:16:05 +0530

    fn & fnSync test cases

--- test/fnSync.test.js
+++ test/fnSync.test.js
@@ -18,19 +18,19 @@ class Cache {
 
 }
 
-test('.fn()', async () => {
+test('.fnSync()', () => {
 
-  const mockFn = jest.fn(async (a, b) => a + b);
+  const mockFn = jest.fn((a, b) => a + b);
 
-  const cachedFn = fn(mockFn, new Cache(), (...args) => args.join('-'));
+  const cachedFn = fnSync(mockFn, new Cache(), (...args) => args.join('-'));
 
-  expect(await cachedFn(1, 2)).toBe(3);
+  expect(cachedFn(1, 2)).toBe(3);
   expect(mockFn.mock.calls).toHaveLength(1);
 
-  expect(await cachedFn(1, 2)).toBe(3);
+  expect(cachedFn(1, 2)).toBe(3);
   expect(mockFn.mock.calls).toHaveLength(1);
 
-  expect(await cachedFn(3, 4)).toBe(7);
+  expect(cachedFn(3, 4)).toBe(7);
   expect(mockFn.mock.calls).toHaveLength(2);
 
 });
